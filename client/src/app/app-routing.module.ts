1f0e0d638b16ed3583dfceee591dce7d2d6c5ab6
fee8ack21 <fee8ack21@gmail.com>
2021-11-06 16:37:28 +0800

    商品元件建立及路由初始化

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailComponent } from './views/product/product-detail/product-detail.component';
+import { ProductListComponent } from './views/product/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'product/:productType', component: ProductListComponent },
+  { path: 'product/:productType/:productID', component: ProductDetailComponent },
+  { path: '', redirectTo: '/product', pathMatch: 'full' },
+  { path: '**', redirectTo: '/product' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './views/product/product-list/product-list.component';
+import { ProductDetailComponent } from './views/product/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
