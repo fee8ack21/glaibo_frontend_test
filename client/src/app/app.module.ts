12737dceed2cae2f81ec5e086489b790ecabd823
fee8ack21 <fee8ack21@gmail.com>
2021-11-07 02:48:18 +0800

    1.商品列表 / 商品內頁 切版與功能實作 2.routing 調整 3.環境變數新增api 路徑 4.server 假資料調整

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './views/product/product-list/product-list.component';
 import { ProductDetailComponent } from './views/product/product-detail/product-detail.component';
+import { LoaderComponent } from './shared/components/loader/loader.component';
+import { LoaderService } from './shared/services/loader.service';
+import { TabNavbarComponent } from './shared/components/tab-navbar/tab-navbar.component';
+import { TabNavbarModalComponent } from './shared/components/tab-navbar/tab-navbar-modal/tab-navbar-modal.component';
+import { HttpService } from './shared/services/http.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    LoaderComponent,
+    TabNavbarComponent,
+    TabNavbarModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [LoaderService, HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
