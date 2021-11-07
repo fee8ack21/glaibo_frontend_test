f9135958fa6a44c3b0d9401ab66694a8844757f3
fee8ack21 <fee8ack21@gmail.com>
2021-11-07 22:05:19 +0800

    商品內頁放大鏡功能

--- client/src/app/views/product/product-detail/product-detail.component.ts
+++ client/src/app/views/product/product-detail/product-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/shared/models/product';
 import { HttpService } from 'src/app/shared/services/http.service';
@@ -10,28 +10,20 @@ import { LoaderService } from 'src/app/shared/services/loader.service';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-  product: Product = { id: 0, name: '', price: 0, type: 0, image: '' };
+  product: Product = { id: '', name: '', price: 0, type: 0, typeName: '', image: '' };
   productType: number;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private httpService: HttpService,
-    private loaderService: LoaderService
+    private loaderService: LoaderService,
+    private elements: ElementRef
   ) { }
 
   ngOnInit(): void {
     let productID = this.route.snapshot.params['productID'];
     this.productType = this.route.snapshot.params['productType'];
-
-    if (isNaN(this.productType)) {
-      this.router.navigate(['/product'])
-      return;
-    }
-    if (isNaN(productID)) {
-      this.router.navigate(['/product', this.productType])
-      return;
-    }
 
     this.loaderService.start();
     this.getProduct(productID).then(() => {
@@ -40,6 +32,8 @@ export class ProductDetailComponent implements OnInit {
       alert(error);
       this.loaderService.stop();
     });
+
+    this.magnifier();
   }
 
   getProduct(id: number) {
@@ -54,5 +48,46 @@ export class ProductDetailComponent implements OnInit {
         }
       );
     })
+  }
+
+  magnifier() {
+    var productImage = this.elements.nativeElement.querySelector('.product-image');
+    var magnifier = this.elements.nativeElement.querySelector('.product-image__magnifier');
+
+    productImage.addEventListener('mouseenter', enterHandler)
+    productImage.addEventListener('mousemove', moveHandler)
+    productImage.addEventListener('mouseleave', leaveHandler)
+
+    function enterHandler(e: any) {
+      magnifier.classList.add('product-image__magnifier--show')
+    }
+
+    function moveHandler(e: any) {
+      let rect = productImage.getBoundingClientRect()
+      let offsetX, offsetY
+
+      offsetX = e.clientX - rect.left;
+      offsetY = e.clientY - rect.top;
+
+      if (offsetX < 0 || offsetY < 0 || offsetX > rect.width || offsetY > rect.height) {
+        magnifier.classList.remove('product-image__magnifier--show')
+        return
+      }
+
+      let distanceX = offsetX / rect.width
+      let distanceY = offsetY / rect.height
+
+      let magnifierWidth = magnifier.offsetWidth;
+      let magnifierHeight = magnifier.offsetHeight;
+
+      magnifier.style.setProperty('--distance-x', distanceX)
+      magnifier.style.setProperty('--distance-y', distanceY)
+      magnifier.style["top"] = `calc(${distanceY * 100}% - ${magnifierWidth / 2}px)`;
+      magnifier.style["left"] = `calc(${distanceX * 100}% - ${magnifierHeight / 2}px)`;
+    }
+
+    function leaveHandler(e: any) {
+      magnifier.classList.remove('product-image__magnifier--show')
+    }
   }
 }
